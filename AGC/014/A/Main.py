5f11401c6e59b8e5087a5a45a9f37f51623d35fc
giraffe <giraffe@giraffe-no-MacBook-Pro.local>
2017-05-06 23:02:00 +0900

    "yet"

---
+++ AGC/014/A/Main.py
@@ -0,0 +1,23 @@
+n = map(int, raw_input().split())
+m = [0, 0, 0]
+
+
+count = 0
+while True:
+    if n[0] == n[1] and n[1] == n[2]:
+        print -1
+        exit()
+    for i in range(3):
+        if n[i]%2 == 1:
+            print count
+            exit()
+    for i in range(3):
+        m[i]=(n[(i+1)%3]/2)+(n[(i+2)%3]/2)
+    for i in range(3):
+        n[i] = m[i]
+    count += 1
+
+# a b c
+# b/2+c/2 a/2+c/2 a/2+b/2
+#
+#
