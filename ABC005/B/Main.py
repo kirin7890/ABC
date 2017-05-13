4b2c8d041697b68d871eb693f25690c8c5fbdc7f
giraffe <giraffe@naist-wavenet124-255.naist.jp>
2017-05-13 23:01:38 +0900

    "5/13"

---
+++ ABC005/B/Main.py
@@ -0,0 +1,7 @@
+n = int(raw_input())
+t = []
+
+for i in xrange(n):
+    t[i] = raw_input()
+
+print min(t)
--- AGC/014/A/Main.py
+++ AGC/014/A/Main.py
@@ -4,13 +4,13 @@ m = [0, 0, 0]
 
 count = 0
 while True:
-    if n[0] == n[1] and n[1] == n[2]:
-        print -1
-        exit()
     for i in range(3):
         if n[i]%2 == 1:
             print count
             exit()
+    if n[0] == n[1] and n[1] == n[2]:
+        print -1
+        exit()
     for i in range(3):
         m[i]=(n[(i+1)%3]/2)+(n[(i+2)%3]/2)
     for i in range(3):
